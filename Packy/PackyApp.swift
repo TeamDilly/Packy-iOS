0a6e4552113a7b016111125c22041516ba10c9e6
qwerty3345 <qwerty3345@naver.com>
2024-01-07 15:13:42 +0900

    [SETTING] 프로젝트 초기 생성

---
+++ Packy/PackyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PackyApp.swift
+//  Packy
+//
+//  Created by Mason Kim on 1/7/24.
+//
+
+import SwiftUI
+
+@main
+struct PackyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
