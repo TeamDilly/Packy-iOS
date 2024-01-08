79c68373ac6e5d98d846691171d9b07f85203755
qwerty3345 <qwerty3345@naver.com>
2024-01-09 00:19:01 +0900

    [UPDATE] 디자인 시스템에 따라 Packy Button 구현
    - isPressed, disabled 에 따라서 색상 및 전반적인 디자인 변경

--- Packy/DesignSystem/Components/PackyButton.swift
+++ Packy/DesignSystem/Components/PackyButton.swift
@@ -9,39 +9,128 @@ import SwiftUI
 
 struct PackyButton: View {
 
-    let title: String
-    let action: () -> Void
+    enum SizeType {
+        case large
+        case medium
+        case small
+    }
 
-    var textColor: Color = .white
-    var backgroundColor: Color = .purple500
+    enum ColorType {
+        case purple
+        case black
+        case lightPurple
+    }
+
+    let title: String
+    
+    var sizeType: SizeType = .large
+    var colorType: ColorType = .purple
+
+    let action: () -> Void
 
     var body: some View {
         Button(title, action: action)
             .buttonStyle(
                 PackyButtonStyle(
-                    textColor: textColor,
-                    backgroundColor: backgroundColor
+                    sizeType: sizeType,
+                    colorType: colorType
                 )
             )
     }
 }
 
+// MARK: - Button Style
+
 private struct PackyButtonStyle: ButtonStyle {
     @Environment(\.isEnabled) private var isEnabled
 
-    var textColor: Color
-    var backgroundColor: Color
+    let sizeType: PackyButton.SizeType
+    let colorType: PackyButton.ColorType
 
     func makeBody(configuration: Configuration) -> some View {
+        let isPressed = configuration.isPressed
+
         return configuration.label
-            .packyFont(.body2)
+            .packyFont(packyFont)
             .foregroundColor(textColor)
             .frame(maxWidth: .infinity)
+            .frame(height: height)
             .background {
-                RoundedRectangle(cornerRadius: 16)
-                    .fill(backgroundColor)
-                    .frame(height: 58)
+                RoundedRectangle(cornerRadius: cornerRadius)
+                    .fill(backgroundColor(isPressed: isPressed))
             }
-            .padding(.horizontal, 24)
+
+    }
+}
+
+// MARK: - Inner Properties
+
+private extension PackyButtonStyle {
+    var textColor: Color {
+        guard isEnabled else { return .white }
+        return colorType == .lightPurple ? .purple600 : .white
+    }
+
+    func backgroundColor(isPressed: Bool) -> Color {
+        guard isEnabled else { return .gray300 }
+        switch colorType {
+        case .purple:       return isPressed ? .purple600 : .purple500
+        case .black:        return isPressed ? .gray800 : .black
+        case .lightPurple:  return isPressed ? .purple100 : .purple200
+        }
+    }
+
+    var height: CGFloat {
+        switch sizeType {
+        case .large:    return 58
+        case .medium:   return 50
+        case .small:    return 36
+        }
+    }
+
+    var cornerRadius: CGFloat {
+        switch sizeType {
+        case .large:    return 16
+        case .medium:   return 8
+        case .small:    return 8
+        }
+    }
+
+    var packyFont: PackyFont {
+        switch sizeType {
+        case .large:    return .body2
+        case .medium:   return .body4
+        case .small:    return .body6
+        }
+    }
+}
+
+#Preview {
+    ScrollView {
+        VStack {
+            Text("Large").packyFont(.title1)
+            PackyButton(title: "Confirm") {}
+            PackyButton(title: "Confirm", colorType: .black) {}
+            PackyButton(title: "Confirm", colorType: .lightPurple) {}
+            PackyButton(title: "Confirm", colorType: .lightPurple) {}
+                .disabled(true)
+
+            Text("Medium").packyFont(.title1)
+                .padding(.top)
+            PackyButton(title: "Confirm", sizeType: .medium) {}
+            PackyButton(title: "Confirm", sizeType: .medium, colorType: .black) {}
+            PackyButton(title: "Confirm", sizeType: .medium, colorType: .lightPurple) {}
+            PackyButton(title: "Confirm", sizeType: .medium, colorType: .lightPurple) {}
+                .disabled(true)
+
+            Text("Small").packyFont(.title1)
+                .padding(.top)
+            PackyButton(title: "Confirm", sizeType: .small) {}
+            PackyButton(title: "Confirm", sizeType: .small, colorType: .black) {}
+            PackyButton(title: "Confirm", sizeType: .small, colorType: .lightPurple) {}
+            PackyButton(title: "Confirm", sizeType: .small, colorType: .lightPurple) {}
+                .disabled(true)
+        }
+        .padding(.horizontal)
     }
 }
