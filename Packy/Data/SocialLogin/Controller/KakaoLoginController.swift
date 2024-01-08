bbd80efbbff24d8b0fc5c986a396ca4071ebd089
qwerty3345 <qwerty3345@naver.com>
2024-01-08 23:18:38 +0900

    [ADD] 카카오 로그인 컨트롤러 구현
    - 카카오톡 존재, 미존재 시나리오 각각 대응

--- Packy/Data/SocialLogin/Controller/KakaoLoginController.swift
+++ Packy/Data/SocialLogin/Controller/KakaoLoginController.swift
@@ -6,3 +6,89 @@
 //
 
 import Foundation
+import KakaoSDKCommon
+import KakaoSDKAuth
+import KakaoSDKUser
+
+enum KakaoLoginError: Error {
+    case invalidToken
+    case invalidUser
+}
+
+struct KakaoLoginController {
+    func initSDK() {
+        print(APIKey.kakao)
+        KakaoSDK.initSDK(appKey: APIKey.kakao)
+    }
+
+    func handle(url: URL) {
+        guard AuthApi.isKakaoTalkLoginUrl(url) else { return }
+        _ = AuthController.handleOpenUrl(url: url)
+    }
+
+    @MainActor
+    func login() async throws -> SocialLoginInfo {
+        return try await withCheckedThrowingContinuation { continuation in
+            if UserApi.isKakaoTalkLoginAvailable() {
+                loginWithKakaoTalk(continuation)
+            } else {
+                loginWithKakaoAccount(continuation)
+            }
+        }
+    }
+
+    private func loginWithKakaoTalk(_ continuation: CheckedContinuation<SocialLoginInfo, Error>) {
+        UserApi.shared.loginWithKakaoTalk { oauthToken, error in
+            if let error {
+                continuation.resume(throwing: error)
+                return
+            }
+            guard let oauthToken else {
+                continuation.resume(throwing: KakaoLoginError.invalidToken)
+                return
+            }
+
+            fetchUserInfo(continuation, accessToken: oauthToken.accessToken)
+        }
+    }
+
+    private func loginWithKakaoAccount(_ continuation: CheckedContinuation<SocialLoginInfo, Error>) {
+        UserApi.shared.loginWithKakaoAccount { (oauthToken, error) in
+            if let error {
+                continuation.resume(throwing: error)
+                return
+            }
+            guard let oauthToken else {
+                continuation.resume(throwing: KakaoLoginError.invalidToken)
+                return
+            }
+
+            fetchUserInfo(continuation, accessToken: oauthToken.accessToken)
+        }
+    }
+
+    private func fetchUserInfo(_ continuation: CheckedContinuation<SocialLoginInfo, Error>, accessToken: String) {
+        UserApi.shared.me { user, error in
+            if let error {
+                continuation.resume(throwing: error)
+                return
+            }
+
+            guard let user,
+                  let userId = user.id else {
+                continuation.resume(throwing: KakaoLoginError.invalidUser)
+                return
+            }
+
+            let info = SocialLoginInfo(
+                id: String(userId),
+                token: accessToken,
+                name: user.kakaoAccount?.name,
+                email: user.kakaoAccount?.email,
+                loginType: .kakao
+            )
+
+            continuation.resume(returning: info)
+        }
+    }
+}
