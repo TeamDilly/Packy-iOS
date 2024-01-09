7c4ff03ddfd6956d6acf0cfc1f1ab0c65cfebba5
qwerty3345 <qwerty3345@naver.com>
2024-01-09 23:37:06 +0900

    [ADD] 프로필 선택 UI 구현

--- Packy/Screens/Intro/SignUp/Profile/SignUpProfileView.swift
+++ Packy/Screens/Intro/SignUp/Profile/SignUpProfileView.swift
@@ -20,10 +20,36 @@ struct SignUpProfileView: View {
     }
 
     var body: some View {
-        List {
-            Text("Hello, SignUpProfile!")
-            TextField("Binding Text", text: viewStore.$textInput)
+        VStack(spacing: 0) {
+            Text("프로필을 선택해주세요")
+                .packyFont(.heading1)
+                .foregroundStyle(.gray900)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.top, 64)
+                .padding(.bottom, 80)
+
+            VStack(spacing: 40) {
+                Image(.mock)
+                    .resizable()
+                    .frame(width: 160, height: 160)
+                    .mask(Circle())
+
+                HStack(spacing: 16) {
+                    ForEach(0..<4, id: \.self) { _ in
+                        Image(.mock)
+                            .resizable()
+                            .frame(width: 60, height: 60)
+                            .mask(Circle())
+                    }
+                }
+            }
+
+            Spacer()
+
+            PackyNavigationLink(title: "저장", pathState: SignUpNavigationPath.State.termsAgreement())
         }
+        // .animation(.spring, value: viewStore.nickname) // TODO: 프로필 변경에 따라 애니메이션 부옇!
+        .padding(.horizontal, 24)
         .task {
             await viewStore
                 .send(._onAppear)
