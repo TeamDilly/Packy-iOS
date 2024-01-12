9ac0d224072ad7503983182975da48ed45fe0326
qwerty3345 <qwerty3345@naver.com>
2024-01-12 20:10:21 +0900

    [UPDATE] 돌다가 멈출 수 있게 Spinning 구현

--- Packy/DesignSystem/ViewModifiers/SpinningModifier.swift
+++ Packy/DesignSystem/ViewModifiers/SpinningModifier.swift
@@ -6,30 +6,66 @@
 //
 
 import SwiftUI
-
-let defaultSpinningAnimation: Animation = .easeInOut(duration: 20).repeatForever(autoreverses: false)
+import Combine
 
 extension View {
-    func spinning(
-        animation: Animation = defaultSpinningAnimation
-    ) -> some View {
+    func spinning(isOn: Bool = true, degreePerSecond: Double = 30) -> some View {
         modifier(
-            SpinningModifier(animation: animation)
+            SpinningModifier(isOn: isOn, degreePerSecond: degreePerSecond)
         )
     }
 }
 
 struct SpinningModifier: ViewModifier {
-    var animation: Animation
+
+    var isOn: Bool
+
+    init(isOn: Bool, degreePerSecond: Double) {
+        self.isOn = isOn
+        let interval = 1 / degreePerSecond
+        self.timer = Timer.publish(every: interval, on: .main, in: .common).autoconnect()
+    }
+
+    private let timer: Publishers.Autoconnect<Timer.TimerPublisher>
+
     @State private var rotationDegree: Double = 0
+    @State private var amountOfIncrease: Double = 0
 
     func body(content: Content) -> some View {
         content
             .rotationEffect(.degrees(rotationDegree))
             .onAppear {
-                withAnimation(animation) {
-                    rotationDegree = 360
+                amountOfIncrease = isOn ? 1 : 0
+            }
+            .onReceive(timer) { _ in
+                if isOn {
+                    rotationDegree += amountOfIncrease
+                    rotationDegree = rotationDegree.truncatingRemainder(dividingBy: 360)
                 }
             }
+            .onChange(of: isOn) {
+                amountOfIncrease = $1 ? 1 : 0
+            }
+            .onDisappear {
+                timer.upstream.connect().cancel()
+            }
     }
+}
+
+#Preview {
+    struct SampleView: View {
+        @State private var isOn: Bool = true
+        var body: some View {
+            VStack {
+                Button("on/off") {
+                    isOn.toggle()
+                }
+
+                Image(.bell)
+                    .spinning(isOn: isOn)
+            }
+        }
+    }
+
+    return SampleView()
 }
