484c8287c1592df6409a66d15e588bbc5e898a41
qwerty3345 <qwerty3345@naver.com>
2024-01-30 22:32:10 +0900

    [UPDATE] 선물 자세히 보기 레이아웃 구현

--- Packy/Screens/OpenBox/BoxDetail/BoxDetailView.swift
+++ Packy/Screens/OpenBox/BoxDetail/BoxDetailView.swift
@@ -16,38 +16,65 @@ struct BoxDetailView: View {
     private let store: StoreOf<BoxDetailFeature>
     @ObservedObject private var viewStore: ViewStoreOf<BoxDetailFeature>
     @ObservedObject private var player: YouTubePlayer = .init()
-    
+
     init(store: StoreOf<BoxDetailFeature>) {
         self.store = store
         self.viewStore = ViewStore(store, observe: { $0 })
         self.player = .init(stringLiteral: viewStore.youtubeUrl)
     }
-    
+
     var body: some View {
         ZStack {
             // blur 가 가장자리를 하얗게 만들기 때문에 뒤에 black을 깔아줌
             Color.gray900.ignoresSafeArea()
-            
+
             overlayBackground
                 .zIndex(5)
-            
+
             overlayPresentingViews
                 .zIndex(5)
-            
+
             ZStack(alignment: .topTrailing) {
-                
+
                 navigationBar
                     .blur(radius: viewStore.presentingState != .detail ? 3 : 0)
                     .zIndex(2)
-                
+
                 GeometryReader { proxy in
                     ScrollView {
                         mainContentView
                             .frame(height: proxy.size.height)
-                        
-                        mainContentView
-                            .frame(height: proxy.size.height)
-                        
+
+                        VStack {
+                            Image(.giftFrame)
+                                .resizable()
+                                .renderingMode(.template)
+                                .foregroundStyle(.gray950)
+                                .scaledToFit()
+                                .overlay {
+                                    NetworkImage(url: "https://picsum.photos/id/100/200/300")
+                                        .aspectRatio(1, contentMode: .fit)
+                                        .padding(20)
+                                }
+                                .padding(35)
+                                .overlay(alignment: .bottom) {
+                                    Button{
+
+                                    } label: {
+                                        Text("이미지 전체보기")
+                                            .packyFont(.body6)
+                                            .foregroundStyle(.white)
+                                            .frame(width: 141, height: 34)
+                                            .background(
+                                                Capsule()
+                                                    .fill(.black.opacity(0.3))
+                                            )
+                                    }
+                                    .offset(y: -95)
+                                }
+                        }
+                        .frame(height: proxy.size.height)
+
                         ZStack {
                             // TODO: 박스 디자인 띄우기
                             // viewStore.boxId
@@ -59,6 +86,7 @@ struct BoxDetailView: View {
                             // }
                         }
                     }
+                    .scrollTargetBehavior(.paging)
                     .blur(radius: viewStore.presentingState != .detail ? 3 : 0)
                 }
             }
@@ -88,7 +116,7 @@ private extension BoxDetailView {
                 viewStore.send(.binding(.set(\.$presentingState, .detail)))
             }
     }
-    
+
     @ViewBuilder
     var overlayPresentingViews: some View {
         BoxDetailPhotoView(
@@ -96,17 +124,21 @@ private extension BoxDetailView {
             text: "기억나니 우리의 추억"
         )
         .opacity(viewStore.presentingState == .photo ? 1 : 0)
-        .transition(.push(from: .top))
-        
+
         BoxDetailLetterView(
             text: viewStore.letterContent,
             borderColor: .pink300
         )
         .padding(.horizontal, 24)
         .opacity(viewStore.presentingState == .letter ? 1 : 0)
-        .transition(.push(from: .top))
+
+        Rectangle()
+            .fill(.blue.opacity(0.3))
+            .frame(width: 300, height: 300)
+            .padding(.horizontal, 24)
+            .opacity(viewStore.presentingState == .gift ? 1 : 0)
     }
-    
+
     var mainContentView: some View {
         GeometryReader { proxy in
             let screenWidth = proxy.size.width
@@ -117,12 +149,12 @@ private extension BoxDetailView {
                         .foregroundStyle(.white)
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .padding(.horizontal, 72)
-                    
+
                     Rectangle()
                         .fill(.clear)
                         .frame(height: 48)
                 }
-                
+
                 HStack {
                     // 추억 사진
                     PhotoElementView(
@@ -131,9 +163,9 @@ private extension BoxDetailView {
                     ) {
                         viewStore.send(.binding(.set(\.$presentingState, .photo)))
                     }
-                    
+
                     Spacer()
-                    
+
                     // 스티커1
                     StickerElementView(
                         stickerType: .sticker1,
@@ -146,7 +178,7 @@ private extension BoxDetailView {
                 .padding(.leading, 33)
                 .padding(.trailing, 36)
                 .padding(.top, 36)
-                
+
                 HStack {
                     // 스티커2
                     StickerElementView(
@@ -155,9 +187,9 @@ private extension BoxDetailView {
                         screenWidth: screenWidth
                     )
                     .disabled(true)
-                    
+
                     Spacer()
-                    
+
                     // 편지
                     LetterElementView(
                         input: .init(selectedLetterDesign: nil, letter: viewStore.letterContent),
@@ -169,7 +201,7 @@ private extension BoxDetailView {
                 .padding(.leading, 36)
                 .padding(.trailing, 33)
                 .padding(.top, 36)
-                
+
                 // 음악
                 MusicElementView(
                     player: player,
@@ -178,17 +210,17 @@ private extension BoxDetailView {
                 .padding(.horizontal, 32)
                 .padding(.top, 43)
                 .frame(maxWidth: .infinity, alignment: .leading)
-                
+
                 Spacer()
-                
+
                 Button {
-                    
+                    viewStore.send(.binding(.set(\.$presentingState, .gift)))
                 } label: {
                     VStack(spacing: 0) {
                         Image(.arrowUp)
                             .renderingMode(.template)
                             .foregroundStyle(.white)
-                        
+
                         Text("밀어서 선물 확인하기")
                             .foregroundStyle(.white)
                     }
@@ -197,15 +229,15 @@ private extension BoxDetailView {
             }
         }
     }
-    
+
     var navigationBar: some View {
         FloatingNavigationBar(
             leadingAction: {
-                
+
             },
             trailingType: .close,
             trailingAction: {
-                
+
             }
         )
     }
