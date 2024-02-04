e65c20608a68d8051ff96793e60a4a18fc6f530f
qwerty3345 <qwerty3345@naver.com>
2024-02-04 12:36:34 +0900

    [ADD] 홈 화면 UI 구현

--- Packy/Screens/Home/HomeView.swift
+++ Packy/Screens/Home/HomeView.swift
@@ -20,15 +20,104 @@ struct HomeView: View {
     }
 
     var body: some View {
-        List {
-            Text("Hello, Home!")
-            TextField("Binding Text", text: viewStore.$textInput)
+        VStack(spacing: 16) {
+            navigationBar
+
+            RoundedRectangle(cornerRadius: 24)
+                .fill(.black)
+                .frame(height: 320)
+
+            VStack(spacing: 24) {
+                HStack {
+                    Text("주고받은 선물박스")
+                        .packyFont(.heading2)
+                        .foregroundStyle(.gray900)
+
+                    Spacer()
+
+                    // TODO: 네비게이션 링크로 대체
+                    // NavigationLink("더보기", state: )
+                    Button("더보기") {
+
+                    }
+                    .buttonStyle(.text)
+                }
+                .padding(.horizontal, 24)
+
+
+                ScrollView(.horizontal) {
+                    HStack(alignment: .top, spacing: 16) {
+                        ForEach(1...10, id: \.self) { index in
+                            GiftInfoCell(boxUrl: "https://picsum.photos/200", sender: "hello", boxTitle: String(repeating: "선물", count: index))
+                        }
+                    }
+                }
+                .safeAreaPadding(.horizontal, 24)
+                .scrollIndicators(.hidden)
+            }
+            .padding(.vertical, 24)
+            .background(
+                RoundedRectangle(cornerRadius: 24)
+                    .fill(.white)
+            )
+
+
+            Spacer()
         }
+        .padding(.horizontal, 16)
+        .background(.gray100)
         .task {
             await viewStore
                 .send(._onAppear)
                 .finish()
         }
+    }
+}
+
+// MARK: - Inner Views
+
+private extension HomeView {
+    var navigationBar: some View {
+        HStack {
+            Image(.logo)
+
+            Spacer()
+
+            // TODO: 네비게이션 링크로 대체
+            Button {} label: {
+                // NavigationLink(state: ) {
+                Image(.setting)
+            }
+        }
+        .frame(height: 48)
+    }
+}
+
+struct GiftInfoCell: View {
+    var boxUrl: String
+    var sender: String
+    var boxTitle: String
+
+    var body: some View {
+        VStack(spacing: 0) {
+            NetworkImage(url: boxUrl)
+                .mask(RoundedRectangle(cornerRadius: 8))
+                .frame(height: 138)
+                .padding(.bottom, 12)
+
+            Text("From: \(sender)")
+                .packyFont(.body6)
+                .foregroundStyle(.purple500)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.bottom, 4)
+
+            Text(boxTitle)
+                .packyFont(.body3)
+                .foregroundStyle(.gray900)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .lineLimit(2)
+        }
+        .frame(width: 120)
     }
 }
 
