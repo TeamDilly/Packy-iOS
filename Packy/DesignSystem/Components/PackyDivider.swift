630432e89b16f612cf8bfb95c5f4d188ba213034
qwerty3345 <qwerty3345@naver.com>
2024-02-04 18:17:40 +0900

    [ADD] 설정 화면 UI 구현

---
+++ Packy/DesignSystem/Components/PackyDivider.swift
@@ -0,0 +1,20 @@
+//
+//  PackyDivider.swift
+//  Packy
+//
+//  Created by Mason Kim on 2/4/24.
+//
+
+import SwiftUI
+
+struct PackyDivider: View {
+    var body: some View {
+        Rectangle()
+            .frame(height: 1)
+            .foregroundStyle(.gray200)
+    }
+}
+
+#Preview {
+    PackyDivider()
+}
--- Packy/Screens/Home/Setting/SettingView.swift
+++ Packy/Screens/Home/Setting/SettingView.swift
@@ -20,13 +20,97 @@ struct SettingView: View {
     }
 
     var body: some View {
-        List {
-            Text("Hello, Setting!")
+        VStack(spacing: 0) {
+            NavigationBar.onlyBackButton {
+                viewStore.send(.backButtonTapped)
+            }
+            .padding(.top, 8)
+
+            ScrollView {
+                VStack(alignment: .leading, spacing: 24) {
+                    profileView
+
+                    SettingListCell(title: "계정 관리")
+
+                    PackyDivider()
+
+                    SettingListCell(title: "패키 공식 SNS")
+                    SettingListCell(title: "1:1 문의하기")
+                    SettingListCell(title: "패키에게 의견 보내기")
+
+                    PackyDivider()
+
+                    SettingListCell(title: "개인정보처리방침")
+                    SettingListCell(title: "이용약관")
+                    versionView
+
+                    PackyDivider()
+
+                    SettingListCell(title: "로그아웃", showRightIcon: false)
+                }
+                .padding(24)
+
+            }
+
+
+            Spacer()
         }
+        .navigationBarBackButtonHidden(true)
         .task {
             await viewStore
                 .send(._onTask)
                 .finish()
+        }
+    }
+}
+
+// MARK: - Inner Views
+
+private extension SettingView {
+    var profileView: some View {
+        HStack(spacing: 16) {
+            NetworkImage(url: Constants.mockImageUrl)
+                .frame(width: 60, height: 60)
+
+            Text("메이슨")
+                .packyFont(.heading2)
+                .foregroundStyle(.gray900)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+
+    var versionView: some View {
+        HStack(spacing: 8) {
+            Text("버전")
+                .packyFont(.body2)
+                .foregroundStyle(.gray900)
+
+            Text(Constants.appVersion)
+                .packyFont(.body4)
+                .foregroundStyle(.gray600)
+        }
+    }
+}
+
+private struct SettingListCell: View {
+    var title: String
+    var showRightIcon: Bool = true
+
+    var body: some View {
+        HStack {
+            Text(title)
+                .packyFont(.body2)
+                .foregroundStyle(.gray900)
+
+            Spacer()
+
+            if showRightIcon {
+                Image(.arrowRight)
+                    .renderingMode(.template)
+                    .resizable()
+                    .foregroundStyle(.gray600)
+                    .frame(width: 16, height: 16)
+            }
         }
     }
 }
