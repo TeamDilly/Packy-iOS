d1aa395a99049e9208860b7123e91bd288436335
qwerty3345 <qwerty3345@naver.com>
2024-02-05 20:34:14 +0900

    [ADD] 로딩창이 있는 웹뷰 구현

---
+++ Packy/DesignSystem/Components/WebView.swift
@@ -0,0 +1,77 @@
+//
+//  WebView.swift
+//  Packy
+//
+//  Created by Mason Kim on 2/5/24.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: View {
+    let urlString: String
+
+    @State private var isLoading = true
+
+    var body: some View {
+        ZStack {
+            WebViewRepresentable(urlString: urlString, isLoading: $isLoading)
+                .edgesIgnoringSafeArea(.all)
+            if isLoading {
+                ProgressView()
+            }
+        }
+    }
+}
+
+// MARK: - UIViewRepresentable
+
+private struct WebViewRepresentable: UIViewRepresentable {
+    let urlString: String
+    @Binding var isLoading: Bool
+
+    func makeUIView(context: Context) -> WKWebView  {
+        let webView = WKWebView()
+        webView.scrollView.showsHorizontalScrollIndicator = false
+        webView.scrollView.showsVerticalScrollIndicator = false
+        webView.navigationDelegate = context.coordinator
+
+        if let url = URL(string: urlString) {
+            webView.load(URLRequest(url: url))
+        }
+
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {}
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+}
+
+extension WebViewRepresentable {
+    final class Coordinator: NSObject, WKNavigationDelegate {
+        var parent: WebViewRepresentable
+
+        init(_ parent: WebViewRepresentable) {
+            self.parent = parent
+        }
+
+        func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+            parent.isLoading = true
+        }
+
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            parent.isLoading = false
+        }
+
+        func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction) async -> WKNavigationActionPolicy {
+            return .allow
+        }
+    }
+}
+
+#Preview {
+    WebView(urlString: "https://naver.com")
+}
