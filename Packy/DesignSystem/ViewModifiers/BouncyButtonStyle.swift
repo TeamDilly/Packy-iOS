f84e73f6a7cceaea200f08e81f7002cba16663e6
qwerty3345 <qwerty3345@naver.com>
2024-02-07 02:02:26 +0900

    [FIX] BouncyButtonStyle 은 longPressGesture 반영 안하게 변경

--- Packy/DesignSystem/ViewModifiers/BouncyButtonStyle.swift
+++ Packy/DesignSystem/ViewModifiers/BouncyButtonStyle.swift
@@ -11,7 +11,7 @@ extension ButtonStyle where Self == BouncyButtonStyle {
     static var bouncy: BouncyButtonStyle {
         BouncyButtonStyle()
     }
-    
+
     static func bouncy(
         pressedScale: CGFloat = 0.95,
         bounceAnimation: Animation = .spring(duration: 0.3)
@@ -25,28 +25,15 @@ struct BouncyButtonStyle: ButtonStyle {
     var pressedOpacity: CGFloat = 0.9
     var bounceAnimation: Animation = .spring(duration: 0.3)
 
-    @GestureState private var isPressing: Bool = false
-    // @State private var isPressing: Bool = false
-
     func makeBody(configuration: Configuration) -> some View {
         let isPressed = configuration.isPressed
 
-        return configuration.label
-                .scaleEffect(isPressed || isPressing ? pressedScale : 1)
+        return VStack {
+            configuration.label
+                .scaleEffect(isPressed ? pressedScale : 1)
                 .animation(bounceAnimation, value: isPressed)
-                .animation(bounceAnimation, value: isPressing)
-                .opacity(isPressed ? pressedOpacity : 1)
-                // .onTapGesture {}
-                // .onLongPressGesture(perform: {}) {
-                //     isPressing = $0
-                // }
-                .simultaneousGesture(
-                    LongPressGesture()
-                        .updating($isPressing) { currentState, gestureState,
-                            _ in
-                            gestureState = currentState
-                        }
-                )
+        }
+        .opacity(isPressed ? pressedOpacity : 1)
     }
 }
 
@@ -54,7 +41,7 @@ struct BouncyButtonStyle: ButtonStyle {
     ScrollView {
         ForEach(0..<5, id: \.self) { id in
             Button {
-                print("taptap")
+
             } label: {
                 Rectangle()
                     .frame(width: 300, height: 300)
