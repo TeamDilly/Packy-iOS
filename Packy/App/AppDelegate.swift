1ebc7e2af8b159a9890c13de3cf2179615766f4d
qwerty3345 <qwerty3345@naver.com>
2024-06-19 22:14:23 +0900

    [ADD] 딥링크 처리를 위한 BranchSDK 설정

--- Packy/App/AppDelegate.swift
+++ Packy/App/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import SwiftUI
 import Firebase
+import BranchSDK
 
 final class AppDelegate: NSObject, UIApplicationDelegate {
     func application(
@@ -14,6 +15,12 @@ final class AppDelegate: NSObject, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
     ) -> Bool {
         FirebaseApp.configure()
+
+        Branch.getInstance().initSession(launchOptions: launchOptions) { (params, error) in
+            // TODO: Access and use Branch Deep Link data here (nav to page, display content, etc.)
+            print(params as? [String: AnyObject] ?? [:])
+        }
+
         return true
     }
 }
