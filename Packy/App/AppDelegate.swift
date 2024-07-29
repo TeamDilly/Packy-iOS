3730aec01fac6efcd6acd28d809a404455ae63d1
qwerty3345 <qwerty3345@naver.com>
2024-07-30 00:22:24 +0900

    [ADD] Branch NativeLink 대응 코드 추가

--- Packy/App/AppDelegate.swift
+++ Packy/App/AppDelegate.swift
@@ -16,11 +16,31 @@ final class AppDelegate: NSObject, UIApplicationDelegate {
     ) -> Bool {
         FirebaseApp.configure()
 
+        Branch.getInstance().checkPasteboardOnInstall()
+
         Branch.getInstance().initSession(launchOptions: launchOptions) { (params, error) in
             // TODO: Access and use Branch Deep Link data here (nav to page, display content, etc.)
             print(params as? [String: AnyObject] ?? [:])
         }
 
+        return true
+    }
+
+    func application(
+        _ app: UIApplication,
+        open url: URL,
+        options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+        Branch.getInstance().application(app, open: url, options: options)
+        return true
+    }
+
+    func application(
+        _ application: UIApplication,
+        continue userActivity: NSUserActivity,
+        restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void
+    ) -> Bool {
+        Branch.getInstance().continue(userActivity)
         return true
     }
 }
